6e4c9ca3377c745ab7ca25598e380ef8999707af
omarkobro <139379228+omarkobro@users.noreply.github.com>
2024-03-17 09:10:49 +0200

    Update DB_connection.js

--- DB/DB_connection.js
+++ DB/DB_connection.js
@@ -1,7 +1,7 @@
 
 import mongoose from "mongoose"
 let db_connection = async ()=>{
-    await mongoose.connect(process.env.LOCAL_CONNECTION_URL).then((res)=>{
+    await mongoose.connect(process.env.LOCAL_CONNECTION_HOST).then((res)=>{
         console.log("Data base connected successfully");
     }).catch((err)=>{
         console.log("Data base connection failed",err);
